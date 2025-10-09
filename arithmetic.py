d6917c4d6625b4213376a548eb3b14641f93ac59
Tomas Gutierrez <tgut03@gmail.com>
2025-10-09 15:57:22 -0500

    add main()

--- arithmetic.py
+++ arithmetic.py
@@ -1,75 +1,83 @@
 import random
 import time
 
-# operations
-ops = ["+", "-", "×", "÷"]
+def play_game():
+    # operations
+    ops = ["+", "-", "*", "/"]
 
-# time duration
-max_time = input("time [s]: ")
-max_time = time.time() + int(max_time)
+    # time duration
+    seconds = input("time (s): ")
+    max_time = time.time() + int(seconds)
 
-# addition domain
-add_low1 = 2
-add_high1 = 100
-add_low2 = 2
-add_high2 = 100
+    # addition domain
+    add_low1 = 2
+    add_high1 = 100
+    add_low2 = 2
+    add_high2 = 100
 
-# multiplication domain
-multiply_low1 = 2
-multiply_high1 = 12
-multiply_low2 = 2
-multiply_high2 = 100
+    # multiplication domain
+    multiply_low1 = 2
+    multiply_high1 = 12
+    multiply_low2 = 2
+    multiply_high2 = 100
 
-# initialize score
-score = 0
+    # initialize score
+    score = 0
 
-while time.time() < max_time:
-    # randomize operations
-    op = random.choice(ops)
+    while time.time() < max_time:
+        # randomize operations
+        op = random.choice(ops)
 
-    if op == "+":
-        # randomize numbers
-        num1 = random.randint(add_low1, add_high1)
-        num2 = random.randint(add_low2, add_high2)
+        if op == "+":
+            # randomize numbers
+            num1 = random.randint(add_low1, add_high1)
+            num2 = random.randint(add_low2, add_high2)
 
-        # addition solution
-        soln = num1 + num2
-    
-    elif op == "-":
-        # randomize numbers
-        a = random.randint(add_low1, add_high1)
-        b = random.randint(add_low2, add_high2)
+            # addition solution
+            soln = num1 + num2
+        
+        elif op == "-":
+            # randomize numbers
+            a = random.randint(add_low1, add_high1)
+            b = random.randint(add_low2, add_high2)
 
-        # num1 > num2
-        num1 = max(a, b)
-        num2 = min(a, b)
+            # num1 > num2
+            num1 = max(a, b)
+            num2 = min(a, b)
 
-        # subtraction solution
-        soln = num1 - num2
-    
-    elif op == "×":
-        # randomize numbers
-        num1 = random.randint(multiply_low1, multiply_high1)
-        num2 = random.randint(multiply_low2, multiply_high2)
+            # subtraction solution
+            soln = num1 - num2
+        
+        elif op == "*":
+            # randomize numbers
+            num1 = random.randint(multiply_low1, multiply_high1)
+            num2 = random.randint(multiply_low2, multiply_high2)
 
-        # multiplication solution
-        soln = num1 * num2
-    
-    elif op == "÷": 
-        # randomize numbers
-        num2 = random.randint(multiply_low2, multiply_high2)
-        quotient = random.randint(multiply_low1, multiply_high1)
-        num1 = num2 * quotient
+            # multiplication solution
+            soln = num1 * num2
+        
+        elif op == "/": 
+            # randomize numbers
+            num2 = random.randint(multiply_low2, multiply_high2)
+            quotient = random.randint(multiply_low1, multiply_high1)
+            num1 = num2 * quotient
 
-        # division solution
-        soln = num1 // num2
-    
-    # check answer against solution
-    answer = input(f"{num1} {op} {num2} = ")
-    while answer != str(soln) and (time.time() < max_time):
+            # division solution
+            soln = num1 // num2
+        
+        # check answer against solution
         answer = input(f"{num1} {op} {num2} = ")
-    
-    # update score
-    score += 1
-    
-print(f"score: {score}")
+        while answer != str(soln) and (time.time() < max_time):
+            answer = input(f"{num1} {op} {num2} = ")
+        
+        # update score
+        if answer == str(soln):
+            score += 1
+        
+    print(f"score: {score}")
+    return num1, num2, op
+
+def main():
+    play_game()
+
+main()
